f9de5ff57b415c5d9c2a879f5a745b1a5e897e6c
Edward LK <thehomelessredranger@hotmail.com>
2021-11-27 05:13:54 -0500

    Add retrieve date from click

--- src/components/CalenderUser.vue
+++ src/components/CalenderUser.vue
@@ -1,37 +1,66 @@
 <template>
     <div>
-        <Fullcalendar :options="calendarOptions"/>
+        <Fullcalendar
+        :options="calendarOptions"
+        />
     </div>
 </template>
 
 <script>
-import '@fullcalendar/core/vdom'
-import Fullcalendar from "@fullcalendar/vue3"
+import '@fullcalendar/core/vdom';
+import Fullcalendar from "@fullcalendar/vue3";
 import DayGridPlugin from "@fullcalendar/daygrid";
 import TimeGridPlugin from "@fullcalendar/timegrid";
 import InteractionPlugin from "@fullcalendar/interaction";
 
 export default{
     components : {Fullcalendar},
-        data () {
-            return {
-                isShowModal: false,
-                calendarOptions: {
-                    plugins: [DayGridPlugin, TimeGridPlugin, InteractionPlugin],
-                    initialView: "timeGridWeek",
-                    headerToolbar: {
-                        left: "prev,next today",
-                        center: "title",
-                        right: "dayGridMonth,timeGridWeek,timeGridDay",
-                    },
-                
-                },
-        
-            }
 
-    
-    
-    }
+    data: function() {
+        
+        return {  
+            calendarOptions: {
+                plugins: [DayGridPlugin, TimeGridPlugin, InteractionPlugin],
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay',
+                },
+                editable: true,
+                events: this.currentEvents,
+                selectable: true,
+                dateClick: this.handleDateClick,
+                // eventRender: this.events
+                //select: this.handleDateClick                
+            },
+            currentEvents: [
+                    {
+                    id: 'a',
+                    title: 'my event',
+                    start: '2021-11-11'
+                    }
+            ],        
+        }
+    },
+
+    methods: {
+        handleDateClick: function(arg){
+            var dateObject = arg;
+            let event = {
+                title: "test",
+                start: dateObject.dateStr,
+                end: dateObject.dateStr,
+                display: 'auto'
+            }
+            this.currentEvents.push(event)
+            //alert('Clicked on: ' + dateObject.dateStr);
+            console.log(this.currentEvents)
+        },
+        handleSelect: function(arg){
+            var dateObject = arg;
+            console.log(dateObject)
+        },
+    },
 }
 </script>
 
